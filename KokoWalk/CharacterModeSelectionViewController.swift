1b19ea8d1908aca7b0b8277f2cb2d4902e9c16ca
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-09-04 02:21:39 +0900

    Create CharacterModeSelectionViewController and CharacterCell

---
+++ KokoWalk/CharacterModeSelectionViewController.swift
@@ -0,0 +1,119 @@
+/**
+*
+* CharacterModeSelectionViewController.swift
+* KokoWalk
+* Created by Yuki MIZUNO on 2016/09/04.
+*
+* Copyright (c) 2016, Harekaze
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*     and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+*    may be used to endorse or promote products derived from this software
+*    without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+* THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CharacterModeSelectionViewController: UICollectionViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Uncomment the following line to preserve selection between presentations
+		// self.clearsSelectionOnViewWillAppear = false
+
+		// Do any additional setup after loading the view.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	/*
+	// MARK: - Navigation
+
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		// Get the new view controller using [segue destinationViewController].
+		// Pass the selected object to the new view controller.
+	}
+	*/
+
+	// MARK: UICollectionViewDataSource
+
+	override func numberOfSections(in collectionView: UICollectionView) -> Int {
+		// #warning Incomplete implementation, return the number of sections
+		return 1
+	}
+
+
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		// #warning Incomplete implementation, return the number of items
+		return 4
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCell", for: indexPath)
+	
+		// Configure the cell
+	
+		return cell
+	}
+
+	// MARK: UICollectionViewDelegate
+
+	/*
+	// Uncomment this method to specify if the specified item should be highlighted during tracking
+	override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+		return true
+	}
+	*/
+
+	/*
+	// Uncomment this method to specify if the specified item should be selected
+	override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+		return true
+	}
+	*/
+
+	/*
+	// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+	override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+		return false
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+		return false
+	}
+
+	override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+	
+	}
+	*/
+
+}
