37cfc37aefeedc579d68115b92466e6eb99668b5
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-09-10 20:19:47 +0900

    Remove sample code

--- KokoWalk/KokoMainViewController.swift
+++ KokoWalk/KokoMainViewController.swift
@@ -82,9 +82,6 @@ class KokoMainViewController: UIViewController, UICollectionViewDelegate, UIColl
 			if let sceneNode = scene.rootNode as! GameScene? {
 				self.gameScene = sceneNode
 				
-				// Copy gameplay related content over to the scene
-				sceneNode.graphs = scene.graphs
-				
 				// Set the scale mode to scale to fit the window
 				sceneNode.scaleMode = .aspectFill
 				
@@ -93,8 +90,8 @@ class KokoMainViewController: UIViewController, UICollectionViewDelegate, UIColl
 				
 				sceneView.ignoresSiblingOrder = true
 				
-				sceneView.showsFPS = true
-				sceneView.showsNodeCount = true
+//				sceneView.showsFPS = true
+//				sceneView.showsNodeCount = true
 			}
 		}
 
@@ -136,13 +133,11 @@ class KokoMainViewController: UIViewController, UICollectionViewDelegate, UIColl
 	// MARK: UICollectionViewDataSource
 
 	func numberOfSections(in collectionView: UICollectionView) -> Int {
-		// #warning Incomplete implementation, return the number of sections
 		return 1
 	}
 
 
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		// #warning Incomplete implementation, return the number of items
 		return dataSource.count
 	}
 
@@ -171,34 +166,5 @@ class KokoMainViewController: UIViewController, UICollectionViewDelegate, UIColl
 		}
 		gameScene.addCharacter(name: name)
 	}
-
-	/*
-	// Uncomment this method to specify if the specified item should be highlighted during tracking
-	override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-		return true
-	}
-	*/
-
-	/*
-	// Uncomment this method to specify if the specified item should be selected
-	override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-		return true
-	}
-	*/
-
-	/*
-	// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-	override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-		return false
-	}
-
-	override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-		return false
-	}
-
-	override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-	
-	}
-	*/
 
 }
