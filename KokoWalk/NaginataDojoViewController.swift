6f092eef9985e867468d5ba185613ab6a4160cba
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-09-12 19:28:51 +0900

    Create naginata dojo scene

---
+++ KokoWalk/NaginataDojoViewController.swift
@@ -0,0 +1,94 @@
+/**
+*
+* NaginataDojoViewController.swift
+* KokoWalk
+* Created by Yuki MIZUNO on 2016/09/12.
+*
+* Copyright (c) 2016, Harekaze
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*     and/or other materials provided with the distribution.
+*
+* 3. Neither the name of the copyright holder nor the names of its contributors
+*    may be used to endorse or promote products derived from this software
+*    without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+* THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class NaginataDojoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		if let scene = GKScene(fileNamed: "NaginataScene") {
+			
+			// Get the SKScene from the loaded GKScene
+			if let sceneNode = scene.rootNode as! NaginataScene? {
+				
+				// Set the scale mode to scale to fit the window
+				sceneNode.scaleMode = .aspectFill
+				
+				// Present the scene
+				if let view = self.view as! SKView? {
+					view.presentScene(sceneNode)
+					
+					view.ignoresSiblingOrder = true
+					
+					view.showsFPS = true
+					view.showsNodeCount = true
+				}
+			}
+		}
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	override var shouldAutorotate: Bool {
+		return true
+	}
+	
+	override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+		return .landscape
+	}
+	
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+	
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
