adf6f2a304ee88ea2bacd2584403c612087ade62
Yuki MIZUNO <mizuyuu0904@gmail.com>
2017-01-03 19:51:34 +0900

    Update copyright

--- KokoWalk/AppDelegate.swift
+++ KokoWalk/AppDelegate.swift
@@ -4,7 +4,7 @@
 * KokoWalk
 * Created by Yuki MIZUNO on 2016/09/04.
 *
-* Copyright (c) 2016, Harekaze
+* Copyright (c) 2016-2017, Harekaze
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
